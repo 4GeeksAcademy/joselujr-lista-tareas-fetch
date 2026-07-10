12047dc7800f41d59054586817728af70b49d8bc
Jose Rodriguez <masullo.joseluis@gmail.com>
2026-07-10 19:07:04 +0000

    base hecha

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState("")
+	const [tareas, setTareas] = useState([])
+
+
+
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input type="text"
+				value={inputValue}
+				onChange={e => setInputValue(e.target.value)}
+				onKeyDown={e => {
+					if (e.key === "Enter" && inputValue.trim().length > 0) {
+						setTareas([...tareas, inputValue])
+						setInputValue("")
+					}
+				}
+				}
+			/>
+
+			<ul>
+				{
+					tareas.map((tarea, index) => {
+						return (
+							<li key={index}> {tarea} </li>
+						)
+					})
+				}
+
+			</ul>
+
 		</div>
 	);
 };
